74a7141f9d8bdf4c579d5a4ad45d226419bc24e5
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2025-06-15 16:56:59 +0300

    small sound test works

--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cassert>
 #include "bagel.h"
+#include "SDL3/SDL.h"
+#include "SDL3_mixer/SDL_mixer.h"
 using namespace std;
 using namespace bagel;
 
@@ -18,7 +20,50 @@ void test1() {
 	cout << "Test 1 passed\n";
 }
 
+void soundTest(const char* path) {
+	// Define desired audio spec
+	SDL_AudioSpec spec = {
+		.format = SDL_AUDIO_S16,
+		.channels = 2,
+		.freq = 44100,
+		//.samples = 2048,
+	};
+
+	// Open default audio device
+	if (!Mix_OpenAudio(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec)) {
+		SDL_Log("Mix_OpenAudio failed: %s", SDL_GetError());
+		return;
+	}
+
+	// Load music from file
+	Mix_Music* music = Mix_LoadMUS(path);
+	if (!music) {
+		SDL_Log("Mix_LoadMUS failed: %s", SDL_GetError());
+		Mix_CloseAudio();
+		return;
+	}
+
+	// Play music in a loop
+	if (!Mix_PlayMusic(music, -1)) {
+		SDL_Log("Mix_PlayMusic failed: %s", SDL_GetError());
+		Mix_FreeMusic(music);
+		Mix_CloseAudio();
+		return;
+	}
+
+	SDL_Log("Now playing music...");
+
+	// Simulate a short delay to hear the music
+	SDL_Delay(10000);
+
+	// Cleanup
+	Mix_HaltMusic();
+	Mix_FreeMusic(music);
+	Mix_CloseAudio();
+}
+
 void run_tests()
 {
-	test1();
+	soundTest("res/academic tel aviv theme.mp3");
+	soundTest("res/Character Select.mp3");
 }
